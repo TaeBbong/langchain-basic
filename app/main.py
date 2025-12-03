e2dcee757a96921a603baf2487352f59d28e59cf
TaeBbong <mok05289@korea.ac.kr>
2025-12-03 17:13:06 +0900

    feat: implement fastapi

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from app.router import chat_router, plan_router
+
+
+app = FastAPI(title="LangChain Playground API")
+
+app.include_router(chat_router.router)
+app.include_router(plan_router.router)
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
